25c7baa597e9c1217bfa71bdefc19ff2af6a0765
nangtx <nang.xuantran2001@gmail.com>
2024-04-01 17:25:51 +0700

    40% view advanced

---
+++ advanced.php
@@ -0,0 +1,5 @@
+<?php
+    // Include vs Require
+    // Include : when a file is included with the include statement and PHP cannot find it, the script will continue to execute
+    // Require : the echo statement will not be executed because the script execution dies after the require statement returned a fatal error
+?>
